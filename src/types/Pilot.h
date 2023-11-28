a75e5a256f38b9b010dd02799c471b186b4b6d70
LeoKle <leon.kleinschmidt@outlook.de>
2023-11-28 17:58:04 +0100

    add Pilot type

---
+++ src/types/Pilot.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+#include <chrono>
+#include "Ecfmp.h"
+
+namespace vacdm::types
+{
+  static constexpr std::chrono::utc_clock::time_point defaultTime = std::chrono::utc_clock::time_point(std::chrono::milliseconds(-1));
+
+  typedef struct Pilot_t
+  {
+    std::string callsign;
+    std::chrono::utc_clock::time_point lastUpdate;
+
+    bool inactive = false;
+
+    // position data
+
+    double latitude = 0.0;
+    double longitude = 0.0;
+    bool taxizoneIsTaxiout;
+
+    // flightplan & clearance data
+
+    std::string origin;
+    std::string destination;
+    std::string runway;
+    std::string sid;
+    std::string assignedSquawk;
+    std::string currentSquawk;
+    std::string initialClimb;
+
+    // ACDM procedure data
+
+    std::chrono::utc_clock::time_point eobt = defaultTime;
+    std::chrono::utc_clock::time_point tobt = defaultTime;
+    std::string tobt_state;
+    std::chrono::utc_clock::time_point ctot = defaultTime;
+    std::chrono::utc_clock::time_point ttot = defaultTime;
+    std::chrono::utc_clock::time_point tsat = defaultTime;
+    std::chrono::utc_clock::time_point exot = defaultTime;
+    std::chrono::utc_clock::time_point asat = defaultTime;
+    std::chrono::utc_clock::time_point aobt = defaultTime;
+    std::chrono::utc_clock::time_point atot = defaultTime;
+    std::chrono::utc_clock::time_point asrt = defaultTime;
+    std::chrono::utc_clock::time_point aort = defaultTime;
+
+    // ECFMP Measures
+
+    std::vector<EcfmpMeasure> measures;
+
+    // event booking data
+
+    bool hasBooking;
+  } Pilot;
+}
